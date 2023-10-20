d6cc7e8d29a6d948c41574081a318c3c7c082982
Alesandro Diogo <alesandromendesdiogo24@gmail.com>
2023-10-20 11:48:29 +0100

    First Commit

---
+++ ConversorDeMoeda/Conversor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeMoeda
+{
+     public class Conversor
+    {
+       
+       public static double valorCompra;
+
+        public static double ConversorMoeda(double cotacao)
+        {
+            double calc = (valorCompra * cotacao);
+            double resultado = (calc * 6) / 100;
+
+            return resultado + calc;
+        }
+    }
+
+}
--- ConversorDeMoeda/Program.cs
+++ ConversorDeMoeda/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ConversorDeMoeda
+﻿using System.Globalization;
+
+namespace ConversorDeMoeda
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Qual é a cotação do dólar?");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares você vai comprar?");
+            Conversor.valorCompra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double cotDolar = Conversor.ConversorMoeda(cotacao);
+
+            Console.WriteLine("Valor a ser pago em reais= " + cotDolar.ToString("F2", CultureInfo.InvariantCulture));
+
         }
     }
 }
